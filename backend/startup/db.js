ba4608cc0c77556039930375bbb470f0e10d0b20
pera1337 <codename96@gmail.com>
2020-02-24 19:52:03 +0100

    Code formatting

--- backend/startup/db.js
+++ backend/startup/db.js
@@ -1,20 +1,21 @@
 const Sequelize = require("sequelize");
 module.exports = function() {
-	let sequelize;
-	if(process.env.DATABASE_URL) sequelize = new Sequelize(process.env.DATABASE_URL);
-	else{
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST_NAME,
-      port: process.env.DB_PORT,
-      dialect: "mysql",
-      logging: false
-    }
-  );
-	}
+  let sequelize;
+  if (process.env.DATABASE_URL)
+    sequelize = new Sequelize(process.env.DATABASE_URL);
+  else {
+    sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST_NAME,
+        port: process.env.DB_PORT,
+        dialect: "mysql",
+        logging: false
+      }
+    );
+  }
   global.sequelize = sequelize;
   return sequelize;
 };
